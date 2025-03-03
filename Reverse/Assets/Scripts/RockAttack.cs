e99770e949cfc2570d6dba2dbd733eb68523c6e1
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2025-03-03 14:48:34 -0500

    Hellfire Attack Animation

--- Reverse/Assets/Scripts/RockAttack.cs
+++ Reverse/Assets/Scripts/RockAttack.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 
 public class RockAttack : MonoBehaviour
 {
-    Animator animator;
     Collider2D collider;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
-
         collider = GetComponent<BoxCollider2D>();
         collider.enabled = false;
-    }
-
-    private void Update()
-    {
-        // If the animation is finished, destroy itself.
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95f)
-        {
-            GameObject.Destroy(this.gameObject);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,5 +23,11 @@ public class RockAttack : MonoBehaviour
     public void HitAnimEvent()
     {
         collider.enabled = true;
+    }
+
+    // Place on last frame of the animation to destroy game object
+    public void DestroyObjectAnimEvent()
+    {
+        Destroy(this.gameObject);
     }
 }
